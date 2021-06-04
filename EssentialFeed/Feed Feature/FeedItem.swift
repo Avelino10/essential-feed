b2a489a70990d645067bc3c2788b61020452914f
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-06-04 15:00:04 +0100

    Create EssentalFeed project

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Avelino Rodrigues on 04/06/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
