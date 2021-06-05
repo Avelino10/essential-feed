ed3dcbc102a2d87d1b9fc129c18ba5891520cdff
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-06-05 16:09:00 +0100

    Replace optional parents with enum with success/failure cases

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
+public enum HTTPClientResult {
+    case success(HTTPURLResponse)
+    case failure(Error)
+}
+
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemoteFeedLoader {
@@ -26,12 +31,13 @@ public final class RemoteFeedLoader {
     }
 
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error, response in
+        client.get(from: url) { result in
 
-            if response != nil {
-                completion(.invalidData)
-            } else {
-                completion(.connectivity)
+            switch result {
+                case .success:
+                    completion(.invalidData)
+                case .failure:
+                    completion(.connectivity)
             }
         }
     }
