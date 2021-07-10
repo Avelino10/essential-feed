3a52656a19be6f630ad292a51c6e6f1ca1b02c63
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-07-10 12:01:00 +0100

    Local command delivers images on cache less than seven days old

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -31,11 +31,14 @@ public final class LocalFeedLoader {
     }
 
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { error in
-            if let error = error {
-                completion(.failure(error))
-            } else {
-                completion(.success([]))
+        store.retrieve { result in
+            switch result {
+                case let .failure(error):
+                    completion(.failure(error))
+                case let .found(feed, _):
+                    completion(.success(feed.toModels()))
+                case .empty:
+                    completion(.success([]))
             }
         }
     }
@@ -53,3 +56,10 @@ private extension Array where Element == FeedImage {
         map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
     }
 }
+
+private extension Array where Element == LocalFeedImage {
+    func toModels() -> [FeedImage] {
+        map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    }
+}
+
