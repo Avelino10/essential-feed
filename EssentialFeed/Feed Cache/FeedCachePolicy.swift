49db780af708e4120e4540e81524218ec56d955c
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-07-19 21:00:00 +0100

    Separate `Date` extension helpers into distinct contexts to clarify their scope (one is a cache policy specific DSL and the other is just a reusable DSL helper)

--- EssentialFeed/Feed Cache/FeedCachePolicy.swift
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -13,7 +13,7 @@ internal final class FeedCachePolicy {
     private static let calendar = Calendar(identifier: .gregorian)
 
     private static var maxCacheAgeInDays: Int {
-        return 7
+        7
     }
 
     internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
