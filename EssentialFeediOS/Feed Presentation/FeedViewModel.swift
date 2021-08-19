9873f59b0cfdf5c41dc9251442f1d83bd95f5aa5
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-08-19 15:44:47 +0100

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Avelino Rodrigues on 19/08/2021.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
