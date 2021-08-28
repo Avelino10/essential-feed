aa21cd9c817cfe4fbcd5cbecc6c7df129bf977c3
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-08-28 15:44:24 +0100

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Avelino Rodrigues on 28/08/2021.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
