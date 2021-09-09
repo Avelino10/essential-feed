f31febc3f37ca35ccf76c0a11dfdbc0282651224
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-09-09 11:17:58 +0100

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  EssentialApp
+//
+//  Created by Avelino Rodrigues on 09/09/2021.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {}
