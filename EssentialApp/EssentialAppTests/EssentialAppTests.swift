99320630f986d90190b4bc526bfc81c26cd36c07
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-09-09 11:17:58 +0100

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialAppTests/EssentialAppTests.swift
@@ -0,0 +1,11 @@
+//
+//  EssentialAppTests.swift
+//  EssentialAppTests
+//
+//  Created by Avelino Rodrigues on 09/09/2021.
+//
+
+@testable import EssentialApp
+import XCTest
+
+class EssentialAppTests: XCTestCase {}
