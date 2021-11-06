99bcbb063a902d9c41bf4f4616098c0d015d5101
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-11-06 12:20:55 +0000

    - Move `dispatchOnMainQueue` operator to the point of use (before subscription with `sink`) - Check `isMainQueue` instead of just `isMainThread` since there's no guarantee that the main thread is running the main queue - Fixed issue where Combine publisher is not holding a reference to `RemoteFeedLoader` anymore (since iOS 14)

--- EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -13,15 +13,21 @@ import UIKit
 public final class FeedUIComposer {
     private init() {}
 
-    public static func feedComposedWith(feedLoader: @escaping () -> FeedLoader.Publisher, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) -> FeedViewController {
-        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: { feedLoader().dispatchOnMainQueue() })
+    public static func feedComposedWith(
+        feedLoader: @escaping () -> FeedLoader.Publisher,
+        imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher
+    ) -> FeedViewController {
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
         let feedController = makeFeedViewController(
             delegate: presentationAdapter,
             title: FeedPresenter.title)
 
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(
-                controller: feedController, imageLoader: { imageLoader($0).dispatchOnMainQueue() }), loadingView: WeakRefVirtualProxy(feedController), errorView: WeakRefVirtualProxy(feedController))
+                controller: feedController,
+                imageLoader: imageLoader),
+            loadingView: WeakRefVirtualProxy(feedController),
+            errorView: WeakRefVirtualProxy(feedController))
 
         return feedController
     }
