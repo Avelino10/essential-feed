e92402e21583a1e5954f70852f3da33ac0df6a65
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-12-03 18:06:05 +0000

    Add image comments title

--- EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -9,7 +9,10 @@ import Foundation
 
 public final class FeedPresenter {
     public static var title: String {
-        NSLocalizedString("FEED_VIEW_TITLE", tableName: "Feed", bundle: Bundle(for: FeedPresenter.self), comment: "Title for the feed view")
+        NSLocalizedString("FEED_VIEW_TITLE",
+                          tableName: "Feed",
+                          bundle: Bundle(for: Self.self),
+                          comment: "Title for the feed view")
     }
 
     public static func map(_ feed: [FeedImage]) -> FeedViewModel {
