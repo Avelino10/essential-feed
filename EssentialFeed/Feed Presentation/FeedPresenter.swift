bb4d7f51312f64f3615d4a08b3a558fa5cd411d2
Avelino Rodrigues <arodrigues@smithmicro.com>
2021-12-03 16:16:56 +0000

    Rename and move ResourceErrorView and ViewModel to Shared module

--- EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -11,13 +11,9 @@ public protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
 
-public protocol FeedErrorView {
-    func display(_ viewModel: FeedErrorViewModel)
-}
-
 public final class FeedPresenter {
     private let feedView: FeedView
-    private let errorView: FeedErrorView
+    private let errorView: ResourceErrorView
     private let loadingView: ResourceLoadingView
 
     public static var title: String {
@@ -28,7 +24,7 @@ public final class FeedPresenter {
         return NSLocalizedString("GENERIC_CONNECTION_ERROR", tableName: "Shared", bundle: Bundle(for: FeedPresenter.self), comment: "Error message displayed when we can't load the image feed from the server")
     }
 
-    public init(feedView: FeedView, loadingView: ResourceLoadingView, errorView: FeedErrorView) {
+    public init(feedView: FeedView, loadingView: ResourceLoadingView, errorView: ResourceErrorView) {
         self.feedView = feedView
         self.loadingView = loadingView
         self.errorView = errorView
---
+++ EssentialFeed/Shared Presentation/ResourceErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceErrorView.swift
+//  EssentialFeed
+//
+//  Created by Avelino Rodrigues on 03/12/2021.
+//
+
+import Foundation
+
+public protocol ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel)
+}
